846ddb55b6767383a069a63cfed9b99ff9c8fbfc
shanye <shanye.xzq@alibaba-inc.com>
2025-07-01 15:08:02 +0800

    feat!: reconstruct resource alicloud_eci_container_group

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -1,67 +1,118 @@
+provider "alicloud" {
+  region = "eu-central-1"
+}
 
+# VPC & VSwitch
 data "alicloud_eci_zones" "default" {}
 
-data "alicloud_resource_manager_resource_groups" "default" {}
-
-module "vpc" {
-  source             = "alibaba/vpc/alicloud"
-  create             = true
-  vpc_name           = "tf-test-eci"
-  vpc_cidr           = "172.16.0.0/16"
-  vswitch_name       = "tf-test-eci"
-  vswitch_cidrs      = ["172.16.0.0/21"]
-  availability_zones = [data.alicloud_eci_zones.default.zones.0.zone_ids.1]
-}
-resource "alicloud_eip_address" "eip_address" {
-  address_name = "tf-test-eci"
+resource "alicloud_vpc" "default" {
+  vpc_name   = var.name
+  cidr_block = "10.4.0.0/16"
 }
 
 resource "alicloud_security_group" "security_group" {
-  vpc_id = module.vpc.this_vpc_id
-  name   = "tf-eci-test"
+  vpc_id              = alicloud_vpc.default.id
+  security_group_name = "tf-example-eci"
 }
 
-module "example" {
-  source             = "../.."
-  #alicloud_eci_image_cache
+resource "alicloud_vswitch" "default1" {
+  count        = 1
+  vswitch_name = format("${var.name}_%d", count.index + 1)
+  cidr_block   = format("10.4.%d.0/24", count.index + 1)
+  zone_id      = data.alicloud_eci_zones.default.zones[0].zone_ids[1]
+  vpc_id       = alicloud_vpc.default.id
+}
+
+
+data "alicloud_resource_manager_resource_groups" "default" {}
+
+data "alicloud_regions" "default" {
+  current = true
+}
+
+
+# alicloud_eci_image_cache
+resource "alicloud_eip_address" "eip_address" {
+  address_name = "tf-example-eci"
+}
+
+module "image_cache" {
+  source = "../.."
+
   create_image_cache = true
   image_cache_name   = "tf-name"
-  images             = var.images
+  images             = ["registry-vpc.${data.alicloud_regions.default.regions[0].id}.aliyuncs.com/eci_open/nginx:alpine"]
   security_group_id  = alicloud_security_group.security_group.id
-  vswitch_id         = module.vpc.this_vswitch_ids[0]
+  vswitch_id         = alicloud_vswitch.default1[0].id
+  zone_id            = alicloud_vswitch.default1[0].zone_id
   eip_instance_id    = alicloud_eip_address.eip_address.id
-  resource_group_id  = data.alicloud_resource_manager_resource_groups.default.groups.0.id
-  zone_id            = data.alicloud_eci_zones.default.zones.0.zone_ids.1
-  #alicloud_eci_virtual_node
-  virtual_node_name     = "tf-name"
-  create_virtual_node   = true
-  enable_public_network = false
-  kube_config           = var.kube_config
-  taints                = var.taints
-  virtual_node_tags     = {
-    Created = "TF"
-  }
-  #alicloud_eci_container_group
-  create_container_group           = true
-  container_group_name             = "tf-name"
-  container_image                  = var.container_image
-  cpu                              = var.cpu
-  memory                           = var.memory
-  restart_policy                   = var.restart_policy
-  container_name                   = var.container_name
-  container_working_dir            = var.container_working_dir
-  container_image_pull_policy      = var.container_image_pull_policy
-  container_commands               = var.container_commands
-  volume_mounts                    = var.volume_mounts
-  environment_vars                 = var.environment_vars
-  init_container_name              = var.init_container_name
-  init_container_image             = var.init_container_image
-  init_container_image_pull_policy = var.init_container_image_pull_policy
-  init_container_commands          = var.init_container_commands
-  init_container_args              = var.init_container_args
-  volumes                          = var.volumes
-  ports = {
-    port     = 80
-    protocol = "TCP"
-  }
+  resource_group_id  = data.alicloud_resource_manager_resource_groups.default.groups[0].id
+}
+
+# alicloud_eci_container_group
+module "container_group" {
+  source = "../.."
+
+  create_container_group = true
+  container_group_name   = "tf-name"
+  vswitch_id             = alicloud_vswitch.default1[0].id
+  zone_id                = alicloud_vswitch.default1[0].zone_id
+  security_group_id      = alicloud_security_group.security_group.id
+  cpu                    = var.cpu
+  memory                 = var.memory
+  restart_policy         = var.restart_policy
+  containers = [{
+    image             = "registry-vpc.${data.alicloud_regions.default.regions[0].id}.aliyuncs.com/eci_open/nginx:alpine"
+    name              = "nginx"
+    working_dir       = "/tmp/nginx"
+    image_pull_policy = "IfNotPresent"
+    commands          = ["/bin/sh", "-c", "sleep 9999"]
+    volume_mounts = [{
+      mount_path = "/tmp/example"
+      read_only  = false
+      name       = "empty1"
+    }]
+    ports = [{
+      port     = 80
+      protocol = "TCP"
+    }]
+    environment_vars = [{
+      key   = "name"
+      value = "nginx"
+    }]
+    liveness_probe = [{
+      period_seconds        = "5"
+      initial_delay_seconds = "5"
+      success_threshold     = "1"
+      failure_threshold     = "3"
+      timeout_seconds       = "1"
+      exec = [{
+        commands = ["cat /tmp/healthy"]
+      }]
+    }]
+    readiness_probe = [{
+      period_seconds        = "5"
+      initial_delay_seconds = "5"
+      success_threshold     = "1"
+      failure_threshold     = "3"
+      timeout_seconds       = "1"
+      exec = [{
+        commands = ["cat /tmp/healthy"]
+      }]
+    }]
+  }]
+  init_containers = [{
+    name              = "init-busybox"
+    image             = "registry-vpc.${data.alicloud_regions.default.regions[0].id}.aliyuncs.com/eci_open/busybox:1.30"
+    image_pull_policy = "IfNotPresent"
+    commands          = ["echo"]
+    args              = ["hello initcontainer"]
+  }]
+  volumes = [{
+    name = "empty1"
+    type = "EmptyDirVolume"
+    }, {
+    name = "empty2"
+    type = "EmptyDirVolume"
+  }]
 }
--- variables.tf
+++ variables.tf
@@ -1,3 +1,28 @@
+variable "vswitch_id" {
+  description = "The ID of the VSwitch."
+  type        = string
+  default     = null
+}
+
+variable "resource_group_id" {
+  description = "The ID of the resource group."
+  type        = string
+  default     = null
+}
+
+variable "security_group_id" {
+  description = "The ID of the security group."
+  type        = string
+  default     = null
+}
+
+variable "zone_id" {
+  description = "The ID of the zone"
+  type        = string
+  default     = null
+}
+
+
 #alicloud_eci_image_cache
 variable "create_image_cache" {
   description = "Controls if image cache should be created"
@@ -8,7 +33,7 @@ variable "create_image_cache" {
 variable "image_cache_name" {
   description = "The name of the image cache."
   type        = string
-  default     = ""
+  default     = null
 }
 
 variable "images" {
@@ -17,34 +42,10 @@ variable "images" {
   default     = null
 }
 
-variable "vswitch_id" {
-  description = "The ID of the VSwitch."
-  type        = string
-  default     = ""
-}
-
-variable "resource_group_id" {
-  description = "The ID of the resource group."
-  type        = string
-  default     = ""
-}
-
-variable "security_group_id" {
-  description = "The ID of the security group."
-  type        = string
-  default     = ""
-}
-
-variable "zone_id" {
-  description = "The ID of the zone"
-  type        = string
-  default     = ""
-}
-
 variable "eip_instance_id" {
   description = "The instance ID of the Elastic IP Address (EIP). "
   type        = string
-  default     = ""
+  default     = null
 }
 
 # alicloud_eci_virtual_node
@@ -57,7 +58,7 @@ variable "create_virtual_node" {
 variable "virtual_node_name" {
   description = "The name of the virtual node."
   type        = string
-  default     = ""
+  default     = null
 }
 
 variable "enable_public_network" {
@@ -74,8 +75,8 @@ variable "kube_config" {
 
 variable "taints" {
   description = "The taint."
-  type        = map(string)
-  default     = {}
+  type        = list(map(string))
+  default     = []
 }
 
 variable "virtual_node_tags" {
@@ -93,7 +94,7 @@ variable "create_container_group" {
 variable "container_group_name" {
   description = "The name of the container group."
   type        = string
-  default     = ""
+  default     = null
 }
 
 variable "cpu" {
@@ -114,87 +115,114 @@ variable "restart_policy" {
   default     = "Always"
 }
 
-variable "container_image" {
-  description = "The image of the container."
+variable "instance_type" {
+  description = "The instance type of the container group."
   type        = string
-  default     = ""
+  default     = null
 }
 
-variable "container_name" {
-  description = "The name of the container."
-  type        = string
-  default     = ""
+
+variable "image_registry_credential" {
+  description = "The image registry credential."
+  type = list(object({
+    user_name = string
+    password  = string
+    server    = string
+  }))
+  default = []
 }
 
-variable "container_working_dir" {
-  description = "The working directory of the container."
-  type        = string
-  default     = ""
+variable "containers" {
+  description = "The list of containers."
+  type = list(object({
+    name              = string
+    image             = string
+    cpu               = optional(string, null)
+    gpu               = optional(string, null)
+    memory            = optional(string, null)
+    image_pull_policy = optional(string, null)
+    working_dir       = optional(string, null)
+    commands          = optional(list(string), null)
+    args              = optional(list(string), null)
+    ports = optional(list(object({
+      port     = optional(string, null)
+      protocol = optional(string, null)
+    })), [])
+    environment_vars = optional(list(object({
+      key   = optional(string, null)
+      value = optional(string, null)
+      field_ref = optional(list(object({
+        field_path = optional(string, null)
+      })), [])
+    })), [])
+    volume_mounts = optional(list(object({
+      mount_path = optional(string, null)
+      read_only  = optional(bool, null)
+      name       = optional(string, null)
+    })), [])
+    liveness_probe = optional(list(object({
+      period_seconds        = optional(number, null)
+      initial_delay_seconds = optional(number, null)
+      success_threshold     = optional(number, null)
+      failure_threshold     = optional(number, null)
+      timeout_seconds       = optional(number, null)
+      exec = optional(list(object({
+        commands = optional(list(string), null)
+      })), [])
+    })), [])
+    readiness_probe = optional(list(object({
+      period_seconds        = optional(number, null)
+      initial_delay_seconds = optional(number, null)
+      success_threshold     = optional(number, null)
+      failure_threshold     = optional(number, null)
+      timeout_seconds       = optional(number, null)
+      exec = optional(list(object({
+        commands = optional(list(string), null)
+      })), [])
+    })), [])
+  }))
+  default = []
 }
 
-variable "container_image_pull_policy" {
-  description = "The restart policy of the image."
-  type        = string
-  default     = "IfNotPresent"
-}
-
-variable "container_commands" {
-  description = "The commands run by the init container."
-  type        = list(string)
-  default     = []
-}
-
-variable "volume_mounts" {
-  description = "The structure of volumeMounts."
-  type        = map(string)
-  default     = {}
-}
-
-variable "ports" {
-  description = "The structure of port."
-  type        = map(string)
-  default     = {}
-}
-
-variable "environment_vars" {
-  description = "The structure of environmentVars."
-  type        = map(string)
-  default     = {}
-}
-
-variable "init_container_name" {
-  description = "The name of the init container."
-  type        = string
-  default     = ""
-}
-
-variable "init_container_image" {
-  description = "The image of the container."
-  type        = string
-  default     = ""
-}
-
-variable "init_container_image_pull_policy" {
-  description = "The restart policy of the image."
-  type        = string
-  default     = "IfNotPresent"
-}
-
-variable "init_container_commands" {
-  description = "The commands run by the init container."
-  type        = list(string)
-  default     = []
-}
-
-variable "init_container_args" {
-  description = "The arguments passed to the commands."
-  type        = list(string)
-  default     = []
+variable "init_containers" {
+  description = "The list of init containers."
+  type = list(object({
+    name              = optional(string, null)
+    image             = optional(string, null)
+    cpu               = optional(string, null)
+    gpu               = optional(string, null)
+    memory            = optional(string, null)
+    image_pull_policy = optional(string, null)
+    working_dir       = optional(string, null)
+    commands          = optional(list(string), null)
+    args              = optional(list(string), null)
+    ports = optional(list(object({
+      port     = optional(number, null)
+      protocol = optional(string, null)
+    })), [])
+    environment_vars = optional(list(object({
+      key   = optional(string, null)
+      value = optional(string, null)
+      field_ref = optional(list(object({
+        field_path = string
+      })), [])
+    })), [])
+    volume_mounts = optional(list(object({
+      mount_path = optional(string, null)
+      read_only  = optional(bool, null)
+      name       = optional(string, null)
+    })), [])
+  }))
+  default = []
 }
 
 variable "volumes" {
   description = "The list of volumes."
-  type        = map(string)
-  default     = {}
+  type = list(object({
+    name = optional(string, null)
+    type = optional(string, null)
+  }))
+
+  default = []
 }
 
